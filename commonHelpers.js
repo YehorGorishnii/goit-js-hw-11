a36125b4fa30f31b8ded5a2560161790b3214d82
YehorGorishnii <131007854+YehorGorishnii@users.noreply.github.com>
2024-03-08 01:36:52 +0000

    Deploying to gh-pages from @ YehorGorishnii/goit-js-hw-11@efb85b905d39d26fd285e6502a789adcd14b580c 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,9 +1,9 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const o of s.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function r(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(e){if(e.ep)return;e.ep=!0;const s=r(e);fetch(e.href,s)}})();function i(a){return a.hits.map(({webformatURL:t,largeImageURL:r,tags:n,likes:e,views:s,comments:o,downloads:l})=>`<div class="gallery-item>
+import{i,S as c}from"./assets/vendor-5b791d57.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();function p(o){return o.hits.map(({webformatURL:s,largeImageURL:r,tags:a,likes:e,views:t,comments:n,downloads:l})=>`<div class="gallery-item>
         <a class="gallery-link" href="${r}">
             <img 
             class="gallery-image" 
-            src="${t}" 
-            alt="${n}" 
+            src="${s}" 
+            alt="${a}" 
             width="360px" height="260px"/>
         </a>
         <div class="gallery-info">
@@ -12,11 +12,11 @@
                 </span>    
             </p>
             <p class="gallery-info-item">
-                <span class="gallery-info-span">Views <span class="tag-span">${s}</span>
+                <span class="gallery-info-span">Views <span class="tag-span">${t}</span>
                 </span>    
             </p>
             <p class="gallery-info-item">
-                <span class="gallery-info-span">Comments <span class="tag-span">${o}</span>
+                <span class="gallery-info-span">Comments <span class="tag-span">${n}</span>
                 </span>    
             </p>
             <p class="gallery-info-item">
@@ -24,5 +24,5 @@
                 </span>    
             </p>
         </div>
-    </div>`).join("")}const c="42701806-8f4bc33de61eec272077c73af",p="https://pixabay.com/api/";function f(a){const t=`${p}?key=${c}&q=${a}&image_type=photo&orientation=horizontal&savesearch=true`;return fetch(t).then(r=>{if(!r.ok)throw new Error(`Response error ${r.status}`);return r.json()}).then(r=>(r.hits.length===0&&console.log("Sorry, there are no images matching your search query. Please try again!"),r)).catch(r=>console.log(`Error: ${r}`))}const u=document.querySelector(".form"),g=document.querySelector(".gallery");u.addEventListener("submit",y);function y(a){a.preventDefault();const t=a.currentTarget,r=t.elements.query.value.trim();if(r===""){console.log("error");return}f(r).then(n=>{g.innerHTML=i(n),t.reset()}).catch(n=>{console.error("Error:",n)})}
+    </div>`).join("")}const u="42701806-8f4bc33de61eec272077c73af",g="https://pixabay.com/api/";function f(o){const s=`${g}?key=${u}&q=${o}&image_type=photo&orientation=horizontal&savesearch=true`;return fetch(s).then(r=>{if(!r.ok)throw new Error(`Response error ${r.status}`);return r.json()}).then(r=>r.hits.length===0?i.error({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#FFFFFF",timeout:4e3,position:"topRight"}):r).catch(r=>console.log(`Error: ${r}`))}const m=document.querySelector(".form"),y=document.querySelector(".gallery");let h=new c(".gallery div ",{captionsData:"alt",captionDelay:250});m.addEventListener("submit",d);function d(o){o.preventDefault();const s=o.currentTarget,r=s.elements.query.value.trim();if(r==="")return i.error({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#FFFFFF",timeout:4e3,position:"topRight"});f(r).then(a=>{y.innerHTML=p(a),s.reset(),h.refresh()}).catch(a=>{console.error("Error:",a)})}
 //# sourceMappingURL=commonHelpers.js.map
